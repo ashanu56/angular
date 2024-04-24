7e25260aae1ca21ea9b8ce1e481709a5a38cbb90
Deepansh Agrawal <deepansh.agrawal@radixweb.com>
2024-04-24 12:14:31 +0530

    angular functions and mouse events.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  yourName = 'Deepansh'
+  currentVal = ""
+  /**
+   * Need to define the data type of parameter
+   * no need to write function keyword
+   * @param name 
+   */
+
+  getName(name: string){ 
+    alert(name+" function called");
+  }
+
+  myEvent(evt: string){
+    console.log(evt);
+    this.currentVal = evt;
+  }
 }
