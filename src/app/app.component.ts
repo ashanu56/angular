77efc6d2f351b56b208c937d52e0a7b8f58f26bf
Deepansh Agrawal <deepansh.agrawal@radixweb.com>
2024-04-24 13:35:35 +0530

    display using conditions in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
+    title = 'angular';
+    show = true;
+    showString = 'yes';
+    showColor = 'red';
+    changeCondition() {
+        this.show = !this.show;
+        var showString = 'no';
+        if (this.show) {
+            var showString = 'yes';
+        }
+        this.showString = showString;
+    }
 }
