b729006c91928bd609b5d441795b6833c579dbbe
Deepansh Agrawal <deepansh.agrawal@radixweb.com>
2024-04-25 14:31:08 +0530

    bootstrap alerts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
 
+const ALERTS = [
+    {
+        type: 'success',
+        message: 'This is an success alert',
+    },
+    {
+        type: 'info',
+        message: 'This is an info alert',
+    },
+    {
+        type: 'warning',
+        message: 'This is a warning alert',
+    },
+    {
+        type: 'danger',
+        message: 'This is a danger alert',
+    },
+    {
+        type: 'primary',
+        message: 'This is a primary alert',
+    },
+    {
+        type: 'secondary',
+        message: 'This is a secondary alert',
+    },
+    {
+        type: 'light',
+        message: 'This is a light alert',
+    },
+    {
+        type: 'dark',
+        message: 'This is a dark alert',
+    },
+];
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
+    title = 'angular';
+    alert = ALERTS.slice() // copy array
+    close(alert:any) {
+		this.alert.splice(this.alert.indexOf(alert), 1);
+	}
+    reset(){
+        this.alert = Array.from(ALERTS);
+    }
 }
