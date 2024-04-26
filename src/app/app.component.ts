f6c4a72cff7b8affb1791ab8901ab78ceff5b718
Deepansh Agrawal <deepansh.agrawal@radixweb.com>
2024-04-26 17:29:06 +0530

    services in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { UserDataService } from './user-data.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  name = "";
+  constructor(private user: UserDataService) {
+    this.name = this.user.getData().name;
+  }
 }
