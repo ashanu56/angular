85c2c253d91e8ff7f705ecebed121a044001a07d
Deepansh Agrawal <deepansh.agrawal@radixweb.com>
2024-04-26 10:52:40 +0530

    routing in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component'
+import { AdminComponent } from './admin/admin.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users',
+    component: UsersComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
